f383ce99ac67d14ea785aa255857f82fbd010ad6
Matheus <pereiralimamatheuspl@gmail.com>
2024-02-03 09:24:16 -0300

    0

--- commit.sh
+++ commit.sh
@@ -3,9 +3,9 @@ PATH_STAY="bash/"
 PATH_COMMIT="$HOME/$PATH_STAY"
 
 GIT_COMMIT () {
-    read -p "DIGITE ALGO:"
     git add "$PATH_COMMIT"
-    git commit -m "learn commit"
+    read -p "DIGITE ALGO:"
+    git commit -m "$?"
     git push -u origin main
 
 }
